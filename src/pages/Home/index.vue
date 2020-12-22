cab278205c57bdef922be492be1afa9eec4ae77c
xiangyang <1256599321@qq.com>
2020-12-22 23:30:34 +0800

    实现Home静态组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    路由组件部分
+    <TypeNav/>
+    <List/>
+    <Recommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
+import TypeNav from './typeNav'
+import List from './List'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
   name: 'Home',
+  components:{
+     TypeNav,
+     List,
+     Recommend,
+     Rank,
+     Like,
+     Floor,
+     Brand
+  },
   data() {
       return {
           
