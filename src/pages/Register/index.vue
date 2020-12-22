20fdc9a714ff57fe20f2c0dcf8c509461b1238ff
xiangyang <1256599321@qq.com>
2020-12-22 10:34:48 +0800

    组件名

---
+++ src/pages/Register/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Register</div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+      return {
+          
+      }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Search</div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+      return {
+          
+      }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
